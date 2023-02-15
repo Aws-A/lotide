bf9f4898dc0644d7914c523fef12e51f43634829
Aws-star <awsalrahim@yahoo.com>
2023-02-14 20:57:47 -0500

    Update

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,7 +12,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]));
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));
+module.exports = assertArraysEqual;
